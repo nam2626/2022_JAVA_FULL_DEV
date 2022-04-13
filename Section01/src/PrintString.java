73bf07a101893ebfcde896bb8fab34b6f34863b3
NamSangKyu <nam2626@naver.com>
2022-04-13 15:34:55 +0900

    출력문 예제

---
+++ Section01/src/PrintString.java
@@ -0,0 +1,15 @@
+
+public class PrintString {
+
+	public static void main(String[] args) {
+		//"문자열" + "문자열" = "문자열문자열"
+		System.out.println("안녕하세요" + "자바입니다");
+		System.out.println("Hello World1");
+		System.out.println("Hello World1");
+		System.out.println("Hello World1");
+		System.out.println("Hello World1");
+		System.out.println("Hello World1");
+		System.out.println("Hello World1");		
+	}
+
+}
