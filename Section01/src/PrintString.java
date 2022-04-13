05adb07298a4bc51bb010d4d1a02e701c3d59838
Your Name <you@example.com>
2022-04-13 15:51:05 +0900

    변수

--- Section01/src/PrintString.java
+++ Section01/src/PrintString.java
@@ -1,15 +1,19 @@
 
 public class PrintString {
-
+	/*
+	 * 주석 
+	 * 		코드에 설명문을 작성하는 영역
+	 * 		실행이 안되는 코드
+	 */
 	public static void main(String[] args) {
 		//"문자열" + "문자열" = "문자열문자열"
 		System.out.println("안녕하세요" + "자바입니다");
 		System.out.println("Hello World1");
-		System.out.println("Hello World1");
-		System.out.println("Hello World1");
-		System.out.println("Hello World1");
-		System.out.println("Hello World1");
-		System.out.println("Hello World1");		
+		System.out.println("Hello World2");
+		//System.out.println("Hello World3");
+		/*
+		System.out.println("Hello World4");
+		System.out.println("Hello World5");
+		*/
 	}
-
 }
---
+++ Section01/src/VariableEx.java
@@ -0,0 +1,15 @@
+
+public class VariableEx {
+	/*
+	 * 	데이터 형태
+	 * 		정수 : 양수 음수 0
+	 * 		실수 : 정수에 소수점이 추가된 형태
+	 * 		문자 : 한글자
+	 * 		논리 : 참(true), 거짓(false)
+	 */
+	public static void main(String[] args) {
+
+
+	}
+
+}
