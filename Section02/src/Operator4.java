9d62bca6c0be3695877f113cd8af598bf26d8d04
Your Name <you@example.com>
2022-04-14 12:31:47 +0900

    관계 연산자

---
+++ Section02/src/Operator4.java
@@ -0,0 +1,31 @@
+
+public class Operator4 {
+	/*
+	 * 	관계연산자(비교연산자) ---> 부등식
+	 * 		
+	 * 		>	<	>=	<=	==	!=
+	 * 
+	 */
+	public static void main(String[] args) {
+		int n1 = 10, n2 = 7;
+		
+		System.out.println(n1 > n2);
+		System.out.println(n1 < n2);
+		System.out.println(n1 >= n2);
+		System.out.println(n1 <= n2);
+		System.out.println(n1 == n2);
+		System.out.println(n1 != n2);
+		
+		boolean flag = n1 >= 300;
+		System.out.println(flag);
+	}
+
+}
+
+
+
+
+
+
+
+
