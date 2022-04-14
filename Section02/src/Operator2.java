4987ae28f74cea0dda2f40c747976a838437c460
Your Name <you@example.com>
2022-04-14 12:15:17 +0900

    증감 연산자

--- Section02/src/Operator2.java
+++ Section02/src/Operator2.java
@@ -1,6 +1,9 @@
 
 public class Operator2 {
-	
+	/*
+	 * 	복합대입연산자 : 연산 결과를 왼쪽에 있는 변수에 연산 후 저장(누적)
+	 * 		+= 	-=	 *=	 /=	 %=
+	 */
 	public static void main(String[] args) {
 		int n = 10;
 //		n = n + 10;//변수 n에 숫자 10을 더해서 저장
---
+++ Section02/src/Operator3.java
@@ -0,0 +1,35 @@
+
+public class Operator3 {
+	/*
+	 * 	증감 연산자 : 변수의 값을 1씩 증가, 1씩 감소 시키는 연산자
+	 * 			    = 이 없어도 값을 변경시켜서 저장하는 연산자
+	 * 	
+	 * 		num++		++num		--num		num--
+	 * 		전위형	++num, --num	num이 해야될 연산을 하기전에 증가
+	 * 		후위형	num++, num--	num이 해야될 연산을 한후에 증가
+	 */
+	public static void main(String[] args) {
+		int n1, n2;
+		n1 = n2 = 10;
+		
+		System.out.println(n1); //10
+		System.out.println(++n1);//11
+		System.out.println(n1);//11
+		
+		System.out.println("------");
+		
+		System.out.println(n2);//10
+		System.out.println(n2++);//10
+		System.out.println(n2);//11
+		
+		n1++;
+		++n2;
+	}
+
+}
+
+
+
+
+
+
