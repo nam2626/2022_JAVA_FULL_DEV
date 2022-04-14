92170801e060ed89034fd63ec960f49ad411f11f
Your Name <you@example.com>
2022-04-14 11:52:12 +0900

    복합대입 연산자

---
+++ Section02/src/Operator2.java
@@ -0,0 +1,21 @@
+
+public class Operator2 {
+	
+	public static void main(String[] args) {
+		int n = 10;
+//		n = n + 10;//변수 n에 숫자 10을 더해서 저장
+		n += 10;
+		System.out.println(n);
+		n -= 3; //변수 n에 숫자 3을 뺀다음 저장
+		System.out.println(n);
+		n *= 2;
+		System.out.println(n);
+		n /= 4;
+		System.out.println(n);
+		n %= 7;
+		System.out.println(n);
+		
+		
+	}
+
+}
