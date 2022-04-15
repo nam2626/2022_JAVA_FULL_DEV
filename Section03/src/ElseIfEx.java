7301107b48959275096cc5408f479e8e795c7f68
Your Name <you@example.com>
2022-04-15 10:43:11 +0900

    조건문

---
+++ Section03/src/ElseIfEx.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ElseIfEx {
+	/*
+	 *	else if(조건식){
+	 *		실행할 코드1
+	 *		실행할 코드2
+	 *		실행할 코드3
+	 *	}
+	 *	if문으로 구성된 조건문에서
+	 *	두번째 조건문부터는 else if를 이용하여 조건문을 구성
+	 *  else if와 else는 반드시 if문이 있어야 사용이 가능
+	 *  단 else if와 else는 조건문 구성에서 생략이 가능하다. 
+	 */
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("나이 입력 >>> ");
+		int age = sc.nextInt();
+
+		
+	}
+
+}
+
+
+
--- Section03/src/ElseQuest.java
+++ Section03/src/ElseQuest.java
@@ -1,13 +1,22 @@
+import java.util.Scanner;
 
 public class ElseQuest {
 	/*
-	 *	사용자로부터 나이를 입력 받으서
-	 *  입력받은 나이가 20세 이상이면 성인 이라고 출력
-	 *  입력받은 나이가 20세 미만이면 미성년자 라고 출력
-	 *  if - else를 이용 
+	 * 사용자로부터 나이를 입력 받아서 
+	 * 입력받은 나이가 20세 이상이면 성인 이라고 
+	 * 출력 입력받은 나이가 20세 미만이면 미성년자 라고 출력
+	 * if - else를 이용
 	 */
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("나이 입력 >>> ");
+		int age = sc.nextInt();
 
+		if (age > 19) {
+			System.out.println("성인 입니다.");
+		} else {
+			System.out.println("미성년자 입니다.");
+		}
 	}
 
 }
