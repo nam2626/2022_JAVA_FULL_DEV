1b5b86cfc7dfd1c7368cbd4f64732b443adad45f
Your Name <you@example.com>
2022-04-15 12:14:24 +0900

    비트 연산자

---
+++ Section03/src/BitOperator.java
@@ -0,0 +1,30 @@
+
+public class BitOperator {
+
+	public static void main(String[] args) {
+		int n1 = 13, n2 = 7;
+		
+		//bit and operator
+		System.out.println(n1 & n2);
+		//bit or operator
+		System.out.println(n1 | n2);
+		//bit xor operator
+		System.out.println(n1 ^ n2);
+		//bit not operator
+		System.out.println(~n1);
+		//l-shift operator
+		System.out.println(n1 << 1);
+		System.out.println(n1 << 2);
+		//r-shift operator
+		System.out.println(n1 >> 1);
+		System.out.println(n1 >> 2);
+		System.out.println(n1 >> 3);
+		System.out.println(n1 >> 4);
+		System.out.println(-1 << 4);
+	}
+
+}
+
+
+
+
