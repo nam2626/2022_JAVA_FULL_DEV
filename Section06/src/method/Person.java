948cc2127741a11bd8e8e5abff11804148fdfd78
Your Name <you@example.com>
2022-04-20 15:28:16 +0900

    메서드 예제

---
+++ Section06/src/method/Person.java
@@ -0,0 +1,39 @@
+package method;
+/*
+ * Person은 이름과 나이, 먹은 음식 내용을 가지고 있음
+ * 생성자에서는 이름과 나이를 외부에서 받아서 초기화
+ * 먹은 음식 내용은 빈문자열("")로 초기화
+ */
+public class Person {
+	String name;
+	int age;
+	String food;
+	
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+		food = "";
+	}
+	/*
+	 * 	메서드(method) - 기능 작성
+	 * 		리턴타입 메서드명([매개변수...]){
+	 * 			실행할 코드;
+	 * 			[return 결과값;]
+	 * 		}
+	 */
+	
+	/*
+	 * 필드의 name과 age를 이용하여 Person에 정보를 출력
+	 * 예시>
+	 * 이름 : OOO
+	 * 나이 : 20
+	 */
+	
+}
+
+
+
+
+
+
+
