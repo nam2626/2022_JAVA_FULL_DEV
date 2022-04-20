6d81f3544bebaea734be6f81f19be7bb9d1b05b2
Your Name <you@example.com>
2022-04-20 16:35:47 +0900

    메서드 예제

--- Section06/src/method/Person.java
+++ Section06/src/method/Person.java
@@ -32,7 +32,40 @@ public class Person {
 		System.out.println("이름 : "+name);
 		System.out.println("나이 : "+age);
 	}
+	/*
+	 * 나이 값을 외부로 부터 받아서 field에 있는
+	 * age에 저장하는 메서드
+	 * 단 나이를 외부에서 받아서 저장할 때는
+	 * 받아온 나이값이 0보다 큰 값만 저장
+	 * 만약에 0이하의 값이 들어오면 저장을 못하게끔 처리
+	 */
+	void setAge(int a) {
+//		if(a > 0)
+//			age = a;
+		if(a < 1)
+			return;//메서드 종료
+		age = a;
+	}
+	/*
+	 * 외부로부터 먹은 음식을 받아서
+	 * food 문자열 끝에 붙여서 저장
+	 * 
+	 * 기존값 "떡볶이"
+	 * 받아온 값 "냉면"
+	 * 
+	 * "떡볶이 냉면" <<-- 저장
+	 */
+	void eat(String f) {
+		food += f + " ";
+	}
+	
+	String getFood() {
+		return food;
+	}
 }
+
+
+
 
 
 
--- Section06/src/method/PersonMain.java
+++ Section06/src/method/PersonMain.java
@@ -4,7 +4,13 @@ public class PersonMain {
 
 	public static void main(String[] args) {
 		Person person = new Person("홍길동", 20);
+		person.setAge(-100);
 		person.printPersonInfo();
+		person.eat("떡볶이");
+		person.eat("냉면");
+		person.eat("김밥");
+		person.eat("돈까스");
+		System.out.println(person.getFood());
 	}
 
 }
