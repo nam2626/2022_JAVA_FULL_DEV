4a7fc28345a6456051aa86c1b12b8d84707aa4d8
Your Name <you@example.com>
2022-04-20 12:31:14 +0900

    생성자 예제

--- Section06/src/constructor/Person.java
+++ Section06/src/constructor/Person.java
@@ -13,13 +13,41 @@ public class Person {
 	/*
 	 * 생성자 : 클래스를 생성할때 제일 먼저 한번만 실행되는 기능
 	 * 		   메서드 명이 클래스명으로 되어있고, 리턴타입이 없음
+	 * 		   객체가 생성될때 제일먼저 해야될 일을 작성하는 부분
 	 */
 	//기본생성자(default constructor)
 	Person(){
 		System.out.println("Person 기본 생성자 호출");
+		name = "홍길동";
+		age = 20;
+		gender = 0;
+	}
+	//해당 생성자는 실행할려면 반드시 문자열을 보내줘야됨
+	Person(String n){
+		System.out.println("Person(String) 생성자 호출");
+		name = n; //외부에서 받아온 문자열을 name에 저장
+	}
+	//나이를 받아서 저장하는 생성자를 작성
+	Person(int i) {
+		System.out.println("Person(int) 생성자 호출");
+		age = i;
+	}
+	
+	Person(String n,int a) {
+		System.out.println("Person(String, int) 생성자 호출");
+		name = n;
+		age = a;
 	}
 	
 }
+
+
+
+
+
+
+
+
 
 
 
--- Section06/src/constructor/PersonMain.java
+++ Section06/src/constructor/PersonMain.java
@@ -3,8 +3,27 @@ package constructor;
 public class PersonMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Person p1 = new Person();
+		System.out.println(p1.name);
+		System.out.println(p1.age);
+		System.out.println(p1.gender);
+		Person p2 = new Person("김철수");
+		System.out.println(p2.name);
+		System.out.println(p2.age);
+		System.out.println(p2.gender);
+		Person p3 = new Person(30);
+		System.out.println(p3.name);
+		System.out.println(p3.age);
+		System.out.println(p3.gender);
+		Person p4 = new Person("김철수", 30);
+		System.out.println(p4.name);
+		System.out.println(p4.age);
+		System.out.println(p4.gender);
 	}
 
 }
+
+
+
+
+
