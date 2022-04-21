b6d311e6fac34743e3862796c0aa6588e5242f58
Your Name <you@example.com>
2022-04-21 15:33:30 +0900

    학생 클래스

---
+++ StudentProject_ver1/src/service/StudentService.java
@@ -0,0 +1,29 @@
+package service;
+
+import java.util.Scanner;
+
+import vo.StudentVO;
+
+public class StudentService {
+	private StudentVO[] arr;
+	private int index;
+	
+	public StudentService() {
+		arr = new StudentVO[10];
+		index=0;
+	}
+	
+	public void registerStudent(Scanner sc) {
+		//학생정보 입력
+		
+		//arr에 입력받은 정보를 이용해서 Student 생성하여 배열에 저장
+		//index 증가
+	}
+}
+
+
+
+
+
+
+
