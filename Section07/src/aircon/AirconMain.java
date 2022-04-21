35434c236ed5c3450b9bf54341a5b8e032a5db9e
Your Name <you@example.com>
2022-04-21 14:24:21 +0900

    클래스 예제

--- Section07/src/aircon/AirconMain.java
+++ Section07/src/aircon/AirconMain.java
@@ -1,10 +1,32 @@
 package aircon;
 
+import java.util.Scanner;
+
 public class AirconMain {
 
 	public static void main(String[] args) {
 		Aircon aircon = new Aircon();
-		aircon.tempUp();
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			System.out.println("1 - 에어컨 전원");
+			System.out.println("2 - 온도 증가");
+			System.out.println("3 - 온도 감소");
+			System.out.println("4 - 바람세기");
+			System.out.println("5 - 운전선택");
+			System.out.println("0 - 프로그램 종료");
+			System.out.print("원하시는 번호를 선택하세요 : ");
+			int command = sc.nextInt();
+			if(command == 0) break;
+			aircon.remote(command);
+		}
+		System.out.println("프로그램 종료");
 	}
 
 }
+
+
+
+
+
+
