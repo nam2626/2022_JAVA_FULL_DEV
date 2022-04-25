316aeee8c290906c2594267004380a3fcdb35d13
Your Name <you@example.com>
2022-04-25 16:49:50 +0900

    상속

--- StudentProject_ver2/src/service/StudentService.java
+++ StudentProject_ver2/src/service/StudentService.java
@@ -5,10 +5,12 @@ import java.util.Scanner;
 import vo.StudentVO;
 
 public class StudentService {
+	private static StudentService instance = new StudentService();
+	
 	private StudentVO[] arr;
 	private int index;
 	
-	public StudentService() {
+	private StudentService() {
 		arr = new StudentVO[10];
 		index=0;
 		//학생 데이터를 4건 추가
@@ -18,29 +20,18 @@ public class StudentService {
 		arr[index++] = new StudentVO("20201114", "박영수", "생활체육학과", 1.56);
 	}
 	
-	public void registerStudent(Scanner sc) {
-		System.out.println("학생 정보 등록을 시작합니다...........");
-		
+	public static StudentService getInstance() {
+		if(instance == null)
+			instance = new StudentService();
+		return instance;
+	}
+
+	public void registerStudent(StudentVO vo) {
 		if(index == arr.length) {
-			System.out.println("더이상 저장할 공간이 없습니다.");
+			System.out.println("더 이상 저장할 공간이 없습니다.");
 			return;
 		}
-		
-		//학생정보 입력
-		System.out.print("학번 : ");
-		String studentNo = sc.nextLine();
-		System.out.print("이름 : ");
-		String name = sc.nextLine();
-		System.out.print("학과 : ");
-		String major = sc.nextLine();
-		System.out.print("평점 : ");
-		double score = sc.nextDouble();
-		sc.nextLine();//엔터를 지움
-		
-		//arr에 입력받은 정보를 이용해서 Student 생성하여 배열에 저장
-		arr[index] = new StudentVO(studentNo, name, major, score);
-		//index 증가
-		index++;
+		arr[index++] = vo;
 		System.out.println("학생정보 등록이 완료되었습니다....");
 	}
 	//전체 학생 정보 조회
