b4c6cd0113ea8207c30df46454aaa311919f8222
Your Name <you@example.com>
2022-04-25 11:10:12 +0900

    상속

---
+++ Section09/src/constructor/Animal.java
@@ -0,0 +1,12 @@
+package constructor;
+
+public class Animal {
+	protected int age;
+
+	public Animal(int age) {
+		super();
+		this.age = age;
+	}
+	
+	
+}
--- Section09/src/inheritance_02/EmployeeMain.java
+++ Section09/src/inheritance_02/EmployeeMain.java
@@ -1,6 +1,7 @@
 package inheritance_02;
 
 public class EmployeeMain {
+	//Employee는 SalaryEmployee, DispatchEmployee까지 올수 있다.(자식 클래스라서)
 	public static void work(Employee e) {
 		e.work();
 	}
@@ -9,6 +10,9 @@ public class EmployeeMain {
 		Employee e1 = new Employee();
 		SalaryEmployee e2 = new SalaryEmployee();
 		DispatchEmployee e3 = new DispatchEmployee();
+		work(e1);
+		work(e2);
+		work(e3);
 	}
 
 }
