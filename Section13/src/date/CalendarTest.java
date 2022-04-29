95b082afdbd4d4b386d4b09337be5f6e5285a294
NamSangKyu <nam2626@naver.com>
2022-04-29 11:53:42 +0900

    Date

--- Section13/src/date/CalendarTest.java
+++ Section13/src/date/CalendarTest.java
@@ -1,10 +1,16 @@
 package date;
 
+import java.util.Calendar;
+
 public class CalendarTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Calendar calendar = Calendar.getInstance();
+		System.out.println(calendar);
+		calendar.set(Calendar.YEAR, 2023);
+		calendar.set(Calendar.MONTH, 11);
+		calendar.set(Calendar.DAY_OF_MONTH, 25);
+		System.out.println(calendar.getTime());
 	}
 
 }
