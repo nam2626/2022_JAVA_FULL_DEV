79711f25dfe367f28e6d473a1dd41ca129a0bed0
NamSangKyu <nam2626@naver.com>
2022-05-03 15:09:25 +0900

    inner class

---
+++ Section14/src/inner/OutterClass.java
@@ -0,0 +1,31 @@
+package inner;
+
+public class OutterClass {
+	private int outter;
+
+	public OutterClass(int outter) {
+		this.outter = outter;
+	}
+	
+	public void printOutter() {
+		System.out.println(outter);
+	}
+	
+	public class InnerClass{
+		private int inner;
+
+		public InnerClass(int inner) {
+			this.inner = inner;
+		}
+		
+		public int sum() {
+			return outter+inner;
+		}
+	}
+}
+
+
+
+
+
+
