0eba4483654fd087eaa5fd9b31b28a2747924aa8
NamSangKyu <nam2626@naver.com>
2022-05-04 14:02:34 +0900

    스레드

---
+++ Section15/src/movie/Cinema.java
@@ -0,0 +1,39 @@
+package movie;
+
+public class Cinema {
+	private String[] seat;
+
+	public Cinema() {
+		seat = new String[5];
+	}
+	
+	public synchronized boolean reservation(int seatNo, String name) {
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		if(seatNo < 1 || seatNo > 5) {
+			System.out.println("올바른 좌석 번호를 입력하세요");
+			return false;
+		}
+		if(seat[seatNo-1] == null) {
+			seat[seatNo-1] = name;
+			System.out.println(name + " 손님 "+seatNo+"좌석 예매에 성공하셨습니다.");
+			return true;
+		}
+		System.out.println(name + " 손님 "+seatNo+"좌석 예매에 실패하셨습니다.");
+		return false;
+	}
+	
+	public void allSeatPrintInfo() {
+		for(int i=0;i<seat.length;i++) {
+			System.out.println(i+1 + " - " + seat[i]);
+		}
+	}
+}
+
+
+
+
+
