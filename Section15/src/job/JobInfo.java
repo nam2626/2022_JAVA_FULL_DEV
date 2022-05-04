b21a5c3dae0bae85c39ecc8b652561fd0b2cd379
NamSangKyu <nam2626@naver.com>
2022-05-04 14:40:54 +0900

    스레드

---
+++ Section15/src/job/JobInfo.java
@@ -0,0 +1,36 @@
+package job;
+
+public class JobInfo {
+	private static JobInfo instance = new JobInfo();
+	private String jobInfo;
+	
+	public JobInfo() {	}
+
+	public static JobInfo getInstance() {
+		if(instance == null)
+			instance = new JobInfo();
+		return instance;
+	}
+	
+	public synchronized String getJobInfo() throws InterruptedException {
+		String msg = null;
+		if(jobInfo == null)
+			wait();//스레드 대기 상태로 만듬
+		msg = jobInfo;
+		jobInfo = null;
+		return msg;
+	}
+	
+	public synchronized void writeJob(String job) {
+		jobInfo = job;
+		notifyAll();
+	}
+	
+	
+}
+
+
+
+
+
+
