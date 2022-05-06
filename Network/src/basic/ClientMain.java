d724944d88556c20cb9878e17aa1921263fb1df2
NamSangKyu <nam2626@naver.com>
2022-05-06 10:20:09 +0900

    Socket Basic

--- Network/src/basic/ClientMain.java
+++ Network/src/basic/ClientMain.java
@@ -1,9 +1,50 @@
 package basic;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class ClientMain {
 
 	public static void main(String[] args) {
-
+		Socket server = null;
+		BufferedReader br = null;
+		PrintWriter pw = null;
+		
+		try {
+			//1. Server Connect - IP Address, port
+			server = new Socket("127.0.0.1",1234);
+			//2. Stream Init
+			pw = new PrintWriter(server.getOutputStream());
+			br = new BufferedReader(new InputStreamReader(server.getInputStream()));
+			//3. Data Send/Receive
+			pw.println("Hello");
+			pw.flush();
+			String str = br.readLine();
+			System.out.println(str);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				//4. Close
+				if(br != null)br.close();
+				if(pw != null)pw.close();
+				if(server != null)server.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
+
+
+
+
+
+
