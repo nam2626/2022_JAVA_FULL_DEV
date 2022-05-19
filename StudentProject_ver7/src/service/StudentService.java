0bf5a79ad2f42a74acb780bf86d9b615ff3a06ba
NamSangKyu <nam2626@naver.com>
2022-05-19 14:35:59 +0900

    jdbc

--- StudentProject_ver7/src/service/StudentService.java
+++ StudentProject_ver7/src/service/StudentService.java
@@ -1,10 +1,17 @@
 package service;
 
+import java.util.ArrayList;
+
+import dao.StudentDAO;
+import exception.StudentException;
+import vo.StudentVO;
+
 public class StudentService {
 	private static StudentService instance = new StudentService();
-
-
+	private StudentDAO dao;
+	
 	private StudentService() {
+		dao = StudentDAO.getInstance();
 	}
 
 	public static StudentService getInstance() {
@@ -13,7 +20,20 @@ public class StudentService {
 		return instance;
 	}
 	
+	public ArrayList<StudentVO> selectAllStudent() throws StudentException{
+		ArrayList<StudentVO> list = dao.selectAllStudent();
+		
+		if(list.isEmpty())
+			throw new StudentException("학생 데이터가 하나도 없습니다.");
+		return list;
+	}
+	
 }
+
+
+
+
+
 
 
 
