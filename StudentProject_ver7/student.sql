71c64cb37b3996bc8b249c7728ab41fe66aa5f25
NamSangKyu <nam2626@naver.com>
2022-05-23 12:16:02 +0900

    jdbc

--- StudentProject_ver7/student.sql
+++ StudentProject_ver7/student.sql
@@ -21,6 +21,65 @@ select s.sno || '\t' || s.sname || '\t' || m.major_name || '\t' || ss.money
 from STUDENT s, MAJOR m, STUDENT_SCHOLARSHIP ss
 where s.major_no = m.major_no(+) and s.sno = ss.student_no;
 
+CREATE OR REPLACE PROCEDURE INSERT_STUDENT
+(
+	IN_SNO IN VARCHAR2,
+	IN_SNAME IN VARCHAR2,
+	IN_MAJOR_NO IN NUMBER,
+	IN_SCORE IN NUMBER
+) AS
+BEGIN
+	INSERT INTO STUDENT VALUES(IN_SNO, IN_SNAME, IN_MAJOR_NO, IN_SCORE);
+	COMMIT;
+	DBMS_OUTPUT.PUT_LINE('정상적으로 데이터가 등록되었습니다.');
+	EXCEPTION
+		WHEN OTHERS THEN
+			DBMS_OUTPUT.PUT_LINE('데이터등록 작업을 실패하였습니다.');
+			ROLLBACK;
+			
+END INSERT_STUDENT;
+
+SET SERVEROUTPUT ON;
+
+EXEC INSERT_STUDENT('AAAA1','BBBBB',21,3.14);
+
+--------------------------------------------------------------------------------------
+CREATE OR REPLACE PROCEDURE INSERT_STUDENT
+(
+	IN_SNO IN VARCHAR2,
+	IN_SNAME IN VARCHAR2,
+	IN_MAJOR_NO IN NUMBER,
+	IN_SCORE IN NUMBER,
+    OUT_RESULT OUT NUMBER
+) AS
+BEGIN
+	INSERT INTO STUDENT VALUES(IN_SNO, IN_SNAME, IN_MAJOR_NO, IN_SCORE);
+	COMMIT;
+	DBMS_OUTPUT.PUT_LINE('정상적으로 데이터가 등록되었습니다.');
+    OUT_RESULT := 1;
+	EXCEPTION
+		WHEN OTHERS THEN
+			DBMS_OUTPUT.PUT_LINE('데이터등록 작업을 실패하였습니다.');
+			ROLLBACK;
+            OUT_RESULT := 0;
+			
+END INSERT_STUDENT;
+--프로시저 실행
+DECLARE
+    RESULT NUMBER;
+BEGIN
+    INSERT_STUDENT('BBBB1','AAAA',21,1.1,RESULT);
+    DBMS_OUTPUT.PUT_LINE(RESULT);
+END;
+
+
+
+
+
+
+
+
+
 
 
 
