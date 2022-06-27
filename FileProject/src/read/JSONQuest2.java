d0a622097badcf2b1de340c042eced27237ac3ec
devcation <88315076+devcation-learn@users.noreply.github.com>
2022-06-27 14:11:01 +0900

    json read

--- FileProject/src/read/JSONQuest2.java
+++ FileProject/src/read/JSONQuest2.java
@@ -5,6 +5,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
 
 import org.json.JSONArray;
@@ -13,24 +15,13 @@ import org.json.JSONObject;
 public class JSONQuest2 {
 
 	public static void main(String[] args) {
-		File file = new File("city.list.json");
-		
-		FileReader fr = null;
-		BufferedReader br = null;
-		
+			
 		try {
-			fr = new FileReader(file);
-			br = new BufferedReader(fr);
-			
-			String r = new String();
-			while(true) {
-				String s = br.readLine();
-				if(s == null) break;
-				r += s;
-			}
-			
+			byte[] encode = Files.readAllBytes(Paths.get("city.list.json"));
+			String r = new String(encode,"UTF-8");
+						
 			JSONArray array = new JSONArray(r);
-			System.out.println(r);
+//			System.out.println(r);
 			for(int i=0;i<array.length();i++) {
 				JSONObject obj = array.getJSONObject(i);
 				if(obj.getString("country").equals("KR")) {
@@ -40,17 +31,8 @@ public class JSONQuest2 {
 					+ " (" + coord.get("lon") + "," + coord.get("lat") +")" );
 				}
 			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
-		}finally {
-			try {
-				if(fr != null)fr.close();
-				if(br != null)br.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
 		}
 	}
 
