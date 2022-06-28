c9dc70898bc4273c2a4fbfdbd4b47da8652b98c5
devcation <88315076+devcation-learn@users.noreply.github.com>
2022-06-28 14:27:51 +0900

    xml reader

--- APITest/src/BlogSearch.java
+++ APITest/src/BlogSearch.java
@@ -110,12 +110,75 @@ public class BlogSearch {
 		}
         return result;
 	}
+	public static ArrayList<HashMap<String, String>> newsSearch(String text) {
+		String clientId = "_1rIIr0u6hwdD4VpqYnd";
+        String clientSecret = "k5ERzutCdQ";
+        String apiURL = "https://openapi.naver.com/v1/search/news.xml";
+        DataOutputStream dos = null;
+        HttpURLConnection con = null;
+        ArrayList<HashMap<String, String>> result = new ArrayList<HashMap<String, String>>();
+        try {
+			text = URLEncoder.encode(text,"UTF-8");
+			
+			String postParams = "?query=" + text;
+			URL url = new URL(apiURL + postParams);
+			con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("GET");
+			con.setRequestProperty("X-Naver-Client-Id", clientId);
+			con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
+			
+			con.setDoOutput(true);
+			
+			DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = builderFactory.newDocumentBuilder();
+			Document document = null;
+			
+			int responseCode = con.getResponseCode();
+			if (responseCode == HttpURLConnection.HTTP_OK) {
+				document = builder.parse(con.getInputStream());
+			} else {  
+				document = builder.parse(con.getErrorStream());
+			}
+
+			document.getDocumentElement().normalize();
+			
+			NodeList nodeList = document.getElementsByTagName("item");
+			for(int i=0;i<nodeList.getLength();i++) {
+				System.out.println(nodeList.item(i).getNodeName());
+				NodeList childNodes = nodeList.item(i).getChildNodes();
+				HashMap<String, String> map = new HashMap<String, String>();
+				for(int j=0;j<childNodes.getLength();j++) {
+					System.out.println("\t" + childNodes.item(j).getNodeName() + " - " + childNodes.item(j).getTextContent());
+					map.put(childNodes.item(j).getNodeName(), childNodes.item(j).getTextContent());
+				}
+				result.add(map);				
+			}
+			
+		} catch (UnsupportedEncodingException e) {
+			throw new RuntimeException("인코딩 실패", e);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		}
+        return result;
+	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("검색어 입력 : ");
 		String text = sc.nextLine();
-        ArrayList<HashMap<String, String>> list = blogSearch(text);
-        writeHTML(list, text);
+        ArrayList<HashMap<String, String>> list1 = blogSearch(text);
+        ArrayList<HashMap<String, String>> list2 = newsSearch(text);
+        HashMap<String, ArrayList<HashMap<String, String>>> map 
+        		= new HashMap<String, ArrayList<HashMap<String,String>>>();
+        map.put("blog", list1);
+        map.put("new", list2);
+        
+        writeHTML(map, text);
     }
 
   
