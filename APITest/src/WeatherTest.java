9a2fdeba278a15f4036908b4b758bf55abb7f0d2
devcation <88315076+devcation-learn@users.noreply.github.com>
2022-06-28 14:41:33 +0900

    xml reader

---
+++ APITest/src/WeatherTest.java
@@ -0,0 +1,53 @@
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URLEncoder;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class WeatherTest {
+
+	public static void main(String[] args) {
+		String result = "";
+        StringBuilder urlBuilder = new StringBuilder(
+                "http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson"); /* URL */
+        try {
+            urlBuilder.append("?" + URLEncoder.encode("serviceKey", "UTF-8") + "=hpOVfNem4MVro1QdBZTMTq%2FMZs%2B8yylSvxNQlqPiEQec%2Bo99WRRbIvrVqLltto5W0TmluoxR7uQHpHFNZ146qg%3D%3D");
+            urlBuilder.append("&" + URLEncoder.encode("pageNo", "UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /* 페이지번호 */
+            urlBuilder.append("&" + URLEncoder.encode("numOfRows", "UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /* 한 페이지 결과 수 */
+            urlBuilder.append("&" + URLEncoder.encode("startCreateDt", "UTF-8") + "=" + URLEncoder.encode("20220301", "UTF-8")); /* 검색할 생성일 범위의 시작 */
+            urlBuilder.append("&" + URLEncoder.encode("endCreateDt", "UTF-8") + "=" + URLEncoder.encode("20220303", "UTF-8")); /* 검색할 생성일 범위의 종료 */
+            DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = builderFactory.newDocumentBuilder();
+            Document document = builder.parse(urlBuilder.toString());
+            document.getDocumentElement().normalize();
+            NodeList tagList = document.getElementsByTagName("item");
+
+            for (int i = 0; i < tagList.getLength(); ++i) {
+                NodeList list = tagList.item(i).getChildNodes();
+                for(int j=0;j < list.getLength();j++) {
+                    result += list.item(j).getNodeName() + " " + list.item(j).getTextContent();
+                    System.out.println(list.item(j).getNodeName() + " " + list.item(j).getTextContent());
+                }
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+	}
+
+}
